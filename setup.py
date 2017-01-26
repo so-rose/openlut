da01b5e910b742318b2cddf4239bc0351087feb3
Sofus Rose <sofus@sofusrose.com>
2017-01-26 16:18:18 -0500

    Removed system pybind11 deps, added LICENCE, other housekeeping.

--- setup.py
+++ setup.py
@@ -3,17 +3,12 @@
 import sys, os
 import os.path as path
 
-#~ from sysconfig import get_python_version, get_path
-
 from setuptools import setup
 from setuptools import Extension
 from setuptools import find_packages
 
-#Weirdly long way to get to the actual header files we need to include.
-#~ pyPath = path.join(path.abspath(get_path('include') + os.sep + '..'), 'site/python{}'.format(get_python_version()))
-
-#Better - Mac & Linux only.
-#~ pyPath = '/usr/local/include/python{}'.format(get_python_version())'
+#pybind11, a dependency, can report its own includes!
+import pybind11
 
 #Make sure we're using gcc.
 os.environ["CC"] = "g++"
@@ -24,7 +19,7 @@ link_args = ['-fopenmp']
 
 olOpt = Extension(	'openlut.lib.olOpt',
 					sources = ['openlut/lib/olOpt.cpp'],
-					#~ include_dirs=[pyPath], #Include from the the python3 source code.
+					include_dirs=[pybind11.get_include()], #Include pybind11.
 					language = 'c++',
 					extra_compile_args = cpp_args,
 					extra_link_args = cpp_args
@@ -43,9 +38,9 @@ setup(	name = 'openlut',
 
 		license = 'MIT Licence',
 		
-		keywords = 'color image images processing',
+		keywords = ['color', 'image', 'images', 'processing'],
 		
-		install_requires = ['numpy', 'wand', 'scipy', 'pygame','PyOpenGL', 'setuptools'],
+		install_requires = ['numpy', 'wand', 'scipy', 'pygame','PyOpenGL', 'setuptools', 'pybind11'],
 		
 		classifiers = [
 			'Development Status :: 3 - Alpha',
