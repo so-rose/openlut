51a524ffa40c4d566a3ef95edb0496af5411b790
Sofus Rose <sofus@sofusrose.com>
2017-01-26 18:35:08 -0500

    Added user feature to upload.sh

--- doc/upload.sh
+++ doc/upload.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-REMOTE=sofus@wakingnexus
+USER="$1"
+if [ -z $USER ]; then USER="sofus"; fi
+
+REMOTE="${USER}@wakingnexus"
 
 #Remove the old docs.
 ssh $REMOTE 'bash -s'  << 'ENDSSH'
@@ -15,7 +18,7 @@ rsync -avzP build/html/* $REMOTE:/var/www/openlut/
 #Set strong permissions.
 ssh $REMOTE 'bash -s'  << 'ENDSSH'
 
-chown -R sofus:www-data /var/www/openlut/*
+chown -R $USER:www-data /var/www/openlut/*
 find /var/www/openlut -type f -exec chmod 0640 {} \;
 find /var/www/openlut -type d -exec chmod 2750 {} \;
 
